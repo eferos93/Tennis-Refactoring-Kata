54165c5d6c6ae54d85604ded9730f1e4ab5ce57a
Eros Fabrici <eros.fabrici@gmail.com>
2020-12-22 16:43:14 +0100

    Fixed primitive obsession problem, refactored the code according to the new abstraction.

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,29 +1,26 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private Player player1, player2;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName.equals("player1"))
+            player1.increaseScore();
         else
-            m_score2 += 1;
+            player2.increaseScore();
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1.getScore() == player2.getScore())
         {
-            switch (m_score1)
+            switch (player1.getScore())
             {
                 case 0:
                         score = "Love-All";
@@ -40,9 +37,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1.getScore() >= 4 || player2.getScore() >= 4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = player1.getScore() - player2.getScore();
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -52,8 +49,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1.getScore();
+                else { score+="-"; tempScore = player2.getScore();}
                 switch(tempScore)
                 {
                     case 0:
