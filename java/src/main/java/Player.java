be81716a59282705f611cab0743f30fd8dd08e94
Eros Fabrici <eros.fabrici@gmail.com>
2020-12-22 18:28:53 +0100

    Added two new methods in Player, by moving from TennisGame1;

--- java/src/main/java/Player.java
+++ java/src/main/java/Player.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 public class Player {
     private String name;
     private int score;
@@ -17,5 +19,29 @@ public class Player {
 
     public void increaseScore() {
         score++;
+    }
+
+    public Optional<Player> playerInAdvantageAgainst(Player otherPlayer) {
+        if (score - otherPlayer.getScore() == 1) {
+            return Optional.of(this);
+        } else if (score - otherPlayer.getScore() == -1){
+            return Optional.of(otherPlayer);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Player> playerWinningAgainst(Player otherPlayer) {
+        if (score - otherPlayer.getScore() >= 2) {
+            return Optional.of(this);
+        } else if (score - otherPlayer.getScore() <= -2) {
+            return Optional.of(otherPlayer);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public boolean isPlayerScoreEqualTo(Player otherPlayer) {
+        return score == otherPlayer.getScore();
     }
 }
--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,11 +1,12 @@
+import java.util.Optional;
 
 public class TennisGame1 implements TennisGame {
 
     public static class Outputs {
         private final static String DEUCE = "Deuce";
         private final static String ALL = "All";
-        private final static String ADVANTAGE = "Advantage for #playerName";
-        private final static String WIN = "Win for #playerName";
+        private final static String ADVANTAGE = "Advantage %s";
+        private final static String WIN = "Win for %s";
         private final static String[] defaults = {"Love", "Fifteen", "Thirty", "Fourty"};
 
     }
@@ -24,22 +25,28 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
+        //String score = "";
         int tempScore=0;
-        if (arePlayerScoresEqual())
+        if (player1.isPlayerScoreEqualTo(player2))
         {
             return finalResultEvenScores(player1.getScore());
         }
         else if (isWinOrTie())
         {
-            int minusResult = player1.getScore() - player2.getScore();
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+
+            Optional<String> score =
+                    player1.playerInAdvantageAgainst(player2).
+                    map(player -> {
+                        return String.format(Outputs.ADVANTAGE, player.getName());
+                    }).or(() -> {
+                       return Optional.of(String.format(Outputs.WIN,
+                               player1.playerWinningAgainst(player2).get().getName()));
+                    });
+            return score.get();
         }
         else
         {
+            String score = "";
             for (int i=1; i<3; i++)
             {
                 if (i==1) tempScore = player1.getScore();
@@ -60,9 +67,11 @@ public class TennisGame1 implements TennisGame {
                         break;
                 }
             }
+            return score;
         }
-        return score;
+        //return score;
     }
+
 
     private boolean isWinOrTie() {
         return player1.getScore() >= 4 || player2.getScore() >= 4;
